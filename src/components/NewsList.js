ed671c8e53514aa0fed6815624543e6c3262031c
Dongyeong Kim <userset301@gmail.com>
2024-09-22 15:13:34 +0900

    Categories

---
+++ src/components/NewsList.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholer.com/160',
+};
+const NewsList = () => {
+  const [articles, setArticle] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=c585c9a8267c44b092c28551dc0b4c58',
+        );
+        setArticle(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem article={sampleArticle} />
+      ))}
+    </NewsListBlock>
+  );
+};
+export default NewsList;
